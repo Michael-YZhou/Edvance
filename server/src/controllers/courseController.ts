80f08fac62372c4ba69eeb9e24fa4dcbb046c75e
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-07-14 16:10:40 +1000

    Create controller function to return courses by given category.

---
+++ server/src/controllers/courseController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import Course from "../models/courseModel";
+
+export const listCourses = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { category } = req.query;
+  try {
+    const courses =
+      category && category !== "all"
+        ? // if category is not all, scan the category column and filter by the category then return the courses
+          await Course.scan("category").eq(category).exec()
+        : // if category is not provided, scan the entire table and return all courses
+          await Course.scan().exec();
+    res
+      .status(200)
+      .json({ message: "courses retrieved successfully", data: courses });
+  } catch (error) {
+    res.status(500).json({ message: "Error retriving courses", error: error });
+  }
+};
