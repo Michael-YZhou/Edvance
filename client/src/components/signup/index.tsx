93ea79a0357142392fe8711fc72d419859fe6c90
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-07-20 17:15:55 +1000

    Set up redirect user between SignIn/SignUp page based on their, or change the SignIn/SignUp component if they are on the CheckOut page. After sign in, redirect user to the courses page based on their roles.

--- client/src/components/signup/index.tsx
+++ client/src/components/signup/index.tsx
@@ -1,8 +1,39 @@
+"use client";
+
 import React from "react";
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, useUser } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { useSearchParams } from "next/navigation";
 
 const SignUpComponent = () => {
+  // this is the user object from Clerk that we use to get the user's email
+  const { user } = useUser();
+  const searchParams = useSearchParams();
+  // this is the url param that we use to check if the user is on the checkout page
+  const isCheckoutPage = searchParams.get("showSignup") !== null;
+  const courseId = searchParams.get("id");
+
+  // if the user is on the checkout page, we let them stay on the checkout page
+  // and signup use the signup component in the checkout page.
+  // otherwise, we redirect them to the signup page
+  const signInUrl = isCheckoutPage
+    ? `/checkout?step=1&id=${courseId}&showSignUp=false` // no need to change the signin component to signup component bacause user wants to signin
+    : "/signin";
+
+  // this is the function that we use to get the redirect url
+  // if the user is on the checkout page, we keep them on the checkout page
+  // otherwise, we redirect them to the courses page based on their user type
+  const getRedirectUrl = () => {
+    if (isCheckoutPage) {
+      return `/checkout?step=2&id=${courseId}`;
+    }
+    const userType = user?.publicMetadata?.userType as string;
+    if (userType === "teacher") {
+      return "/teacher/courses";
+    }
+    return "/user/courses";
+  };
+
   return (
     <SignUp
       appearance={{
@@ -25,6 +56,10 @@ const SignUpComponent = () => {
           footerActionLink: "text-primary-750 hover:text-primary-600",
         },
       }}
+      signInUrl={signInUrl}
+      forceRedirectUrl={getRedirectUrl()}
+      routing="hash"
+      afterSignOutUrl="/"
     />
   );
 };
