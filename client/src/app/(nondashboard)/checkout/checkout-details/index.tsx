c288a5efd7f64e893656f37fae853c9ed3a6ac53
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-07-25 20:33:20 +1000

    Create the CheckOutDetails page and the CoursePreview comp that belongs to it. The Checkout details page is conditionally renderred inside the CheckoutWizard page as step 1.

--- client/src/app/(nondashboard)/checkout/checkout-details/index.tsx
+++ client/src/app/(nondashboard)/checkout/checkout-details/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Loading from "@/components/loading";
 import { useCurrentCourse } from "@/hooks/useCurrentCourse";
 import { useSearchParams } from "next/navigation";
+import CoursePreview from "@/components/course-preview";
 
 const CheckoutDetails = () => {
   const { course: selectedCourse, isLoading, isError } = useCurrentCourse();
@@ -15,7 +16,15 @@ const CheckoutDetails = () => {
   if (isError) return <div>Failed to fetch course data</div>;
   if (!selectedCourse) return <div>Course not found</div>;
 
-  return <div>CheckoutDetails</div>;
+  return (
+    <div className="checkout-details">
+      <div className="checkout-details__container">
+        <div className="checkout-details__preview">
+          <CoursePreview course={selectedCourse} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutDetails;
