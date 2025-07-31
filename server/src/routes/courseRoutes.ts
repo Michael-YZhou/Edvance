a45098b4b5c863a2243ac2c6ff82d33044b65845
Michael <89726435+Michael-YZhou@users.noreply.github.com>
2025-07-31 16:14:45 +1000

    Wire up the updateCourse and deleteCourse routers. Install multer middleware for image and video uploading for updateCourse router.

--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,9 +1,22 @@
 import express from "express";
-import { listCourses, getCourse } from "../controllers/courseController";
+import multer from "multer";
+import {
+  listCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} from "../controllers/courseController";
+import { requireAuth } from "@clerk/express";
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", listCourses);
+router.post("/", requireAuth(), createCourse);
+
 router.get("/:courseId", getCourse);
+router.put("/:courseId", requireAuth(), upload.single("image"), updateCourse); // this is a multer middleware that will upload the image to the server
+router.delete("/:courseId", requireAuth(), deleteCourse);
 
 export default router;
